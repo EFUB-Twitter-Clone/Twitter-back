b9de803073cb1b3e7e01bc4fdcdb6051ff0db6fa
KJI-DEC <kji_dec22@ewhain.net>
2022-05-05 14:27:28 +0900

    feat: add findByUserId function

--- src/main/java/efubtwitter/twitterclone/domain/repository/UserRepository.java
+++ src/main/java/efubtwitter/twitterclone/domain/repository/UserRepository.java
@@ -3,5 +3,8 @@ package efubtwitter.twitterclone.domain.repository;
 import efubtwitter.twitterclone.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserId(String userId);
 }
