3784ae611f070aace8f4b46aa8bb49e85ef79469
Sohyun Park <71393695+Sohyun-Dev@users.noreply.github.com>
2022-05-09 13:29:27 +0900

    Revert "Post"

--- src/main/java/efubtwitter/twitterclone/TwittercloneApplication.java
+++ src/main/java/efubtwitter/twitterclone/TwittercloneApplication.java
@@ -2,9 +2,7 @@ package efubtwitter.twitterclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class TwittercloneApplication {
 
